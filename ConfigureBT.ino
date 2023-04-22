912b00d6d0eb4ffbf3b7ed62c1de38e872a39adc
chris miller <chris.miller.nd@gmail.com>
2023-04-22 17:07:16 -0400

    moved constants to header file

--- ConfigureBT.ino
+++ ConfigureBT.ino
@@ -1,5 +1,6 @@
 /*
  * HC-05/06 AT Command Center
+ *  Version 2.0
  * 
  *  Description: Simple HC05/06 AT configuration program. Requires 2nd UART (Serial1) defined. 
  * 
@@ -20,94 +21,19 @@
  *                Serial writes are asynchronous, so delays must also consider write time
  *                
  *    HC06 connections:
- *      TXD <--> [Serial 1 RX] (pin 19 on Mega)(pin 13 on MKR WiFi board) 
- *      RXD <--> [Serial 1 TX] (pin 18 on Mega)(pin 14 on MKR WiFi board)
+ *                              pin/board   Mega    MKR   Uno WiFi  Zero  Due
+ *      TXD <--> [Serial 1 RX]               19      13      0        0    19
+ *      RXD <--> [Serial 1 TX]               18      14      1        1    18
  *      
  *  Created on: 18-Oct, 2021
  *      Author: miller4@rose-hulman.edu
- *    Modified: 20-Apr, 2023
- *    Revision: 1.5
+ *    Modified: 22-Apr, 2023
+ *    Revision: 2.0
  */
 // uncomment following line to include debugging output to serial
-#define DEBUG           1
+//#define DEBUG           1
 
-#define BAUD_LIST_CNT   9         // count of baud rate options
-#define VERS3_MIN_BAUD  3         // index for firmware 3.x minimum baud rate (4800)
-#define PARITY_LIST_CNT 4         // count of UART parity options
-#define FIRM_UNKNOWN    0         // index for unknown firmware 
-#define FIRM_VERSION1   1         // index for firmware 1.x/2.x models
-#define FIRM_VERSION3   2         // index for firmware 3.x models
-
-#define STOP1BIT        0
-#define STOP2BIT        1
-#define NOPARITY        0
-#define ODDPARITY       1
-#define EVENPARITY      2
-
-#define ENDLINE_NLCR    "\r\n"    // for firmware version 3
-#define ENDLINE_NONE    ""        // for firmware version 1/2
-#define STATUS_OK       "OK"
-#define UART_CMD        "AT+UART="
-#define BAUD_CMD        "AT+BAUD"
-
-#define CONFIG_DELAY    20        // delay for basic configuration changes
-#define FW1_RESPONSE    550       // for firmware version 1/2
-#define FW3_RESPONSE    40        // for firmware version 3
-#define BITS_PER_CHAR   12        // UART frames - worst case: parity, 2 stop bits
-
-/* line ending to be applied to AT commands according to firmware version # */
-int firmVersion = FIRM_UNKNOWN;
-#define VERSION_KNOWN   (firmVersion != FIRM_UNKNOWN)
-
-//String lineEnding = ENDLINE_NLCR;
-char charFromBT;
-int baudRate = VERS3_MIN_BAUD;
-int parity = NOPARITY + 1;
-int stopBits = STOP1BIT;
-int selection;
-//bool found = false;
-String hc06Version = "";
-String nameBT, pin, command;
-
-const unsigned long baudRateList[] = {0, 1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200};
-const uint8_t parityList[] = {0, SERIAL_8N1, SERIAL_8O1, SERIAL_8E1};
-const String parityType[] = {"Invalid", "None", "Odd", "Even"};
-const String parityCmd[] = {"Invalid", "AT+PN", "AT+PO", "AT+PE"};
-const String parityParam[] = {"Invalid", ",0\r\n", ",1\r\n", ",2\r\n"};
-const String stopParam[] = {"", ",0", ",1"};
-const String lineEnding[] = {"", "", "\r\n"};
-const unsigned long responseMS[] = {1, FW1_RESPONSE, FW3_RESPONSE};
-enum HC06commands {LINE_END = 0,
-                  ECHO,
-                  HCVERSION,
-                  BTNAME,
-                  BTPIN,
-                  UART_GET,
-                  BAUD_SET,
-                  PARITY_SET,
-                  OTHER_CMD};
-                  
-const String atCommands[][2] = {{"", "\r\n"},
-                              {"AT", "AT\r\n"},
-                              {"AT+VERSION", "AT+VERSION?\r\n"},
-                              {"AT+NAME", "AT+NAME="},
-                              {"AT+PIN", "AT+PSWD="}};
-//                              {"AT+BAUD", "AT+UART="},
-//                              {"AT+PN", "AT+UART="},
-//                              {"AT+PE", "AT+UART="},
-//                              {"AT+PO", "AT+UART="}};
-
-// Worst-case count of expected characters for response to commands.
-//  Indexed based on HC06commands values.
-const int responseChars[] = {0,
-                            4,
-                            26,
-                            22,
-                            6,
-                            22,
-                            8,
-                            8,
-                            40};
+#include "configureBT.h"
 
 void setup() {
   Serial.begin(57600);
@@ -129,7 +55,6 @@ void loop() {
 
   if (Serial.available() > 0) {
     command = Serial.readString();
-//    selection = command.toInt();
     switch(command.toInt()) {
       case 1:
         setBaudRate();
@@ -513,6 +438,7 @@ void setName() {
 }
 
 void setPin() {
+  String pin;
   if (firmVersion == FIRM_VERSION3) {
     Serial.println("Enter new BT passkey (14 characters max): ");
   } else {
